a0115b66aa07aa1b3da8abbbe8c85264a50c8285
Tony <hrg921@gmail.com>
2021-07-26 12:48:12 +0900

    feat: change hit counter to seeyoufarm

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,9 @@ export default function Home({ recordMap }: Props) {
   return (
     <>
       <HitCounterContainer>
-        <img src="https://hitcounter.pythonanywhere.com/nocount/tag.svg" alt="Hits" />
+        <a href="https://hits.seeyoufarm.com">
+          <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fintroduce-tony.vercel.app&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/>
+          </a>
       </HitCounterContainer>
       <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} pageHeader={<></>} />
     </>
